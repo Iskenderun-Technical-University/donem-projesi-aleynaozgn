e8aacbdf94a179234f1486dd664006bfaf7a72f3
aleynaozgn <aleynur02@gmail.com>
2023-05-14 14:09:10 +0300

    veritabanı oluşturuldu

--- .github/aley/aley/Form1.Designer.cs
+++ .github/aley/aley/Form1.Designer.cs
@@ -28,259 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.sıra_sys = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button10 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(98, 59);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1083, 284);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "sırano";
+            this.columnHeader1.Width = 61;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "ilaçkodu";
+            this.columnHeader2.Width = 117;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "ilaçadı";
+            this.columnHeader3.Width = 134;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "sonkullanmatarihi";
+            this.columnHeader4.Width = 139;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "barkodno";
+            this.columnHeader5.Width = 120;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "fiyat";
+            this.columnHeader6.Width = 101;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "adet";
+            this.columnHeader7.Width = 102;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "üretimfirması";
+            this.columnHeader8.Width = 135;
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "kullanmatalimatı";
+            this.columnHeader9.Width = 167;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(225, 84);
+            this.button1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Location = new System.Drawing.Point(130, 388);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(217, 37);
-            this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Size = new System.Drawing.Size(160, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Görüntüle";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.InfoText;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button2.Location = new System.Drawing.Point(340, 392);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 47);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Kaydet";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(919, 417);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(498, 105);
+            this.label1.Location = new System.Drawing.Point(761, 432);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 16);
-            this.label1.TabIndex = 1;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(225, 142);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(217, 38);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(225, 200);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(217, 39);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(225, 262);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(217, 37);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(225, 321);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(217, 37);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(965, 132);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(217, 37);
-            this.button6.TabIndex = 6;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(965, 190);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(217, 37);
-            this.button7.TabIndex = 7;
-            this.button7.Text = "button7";
-            this.button7.UseVisualStyleBackColor = true;
-            // 
-            // sıra_sys
-            // 
-            this.sıra_sys.AutoSize = true;
-            this.sıra_sys.Location = new System.Drawing.Point(136, 94);
-            this.sıra_sys.Name = "sıra_sys";
-            this.sıra_sys.Size = new System.Drawing.Size(83, 16);
-            this.sıra_sys.TabIndex = 8;
-            this.sıra_sys.Text = "SIRA SAYISI";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(143, 153);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(76, 16);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "İLAÇ KODU";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(159, 211);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 16);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "İLAÇ ADI";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(62, 272);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(157, 16);
-            this.label5.TabIndex = 11;
-            this.label5.Text = "SON KULLANMA TARİHİ";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(115, 331);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(104, 16);
-            this.label6.TabIndex = 12;
-            this.label6.Text = "BARKOD KODU";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(911, 142);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(48, 16);
-            this.label7.TabIndex = 13;
-            this.label7.Text = "FİYATI";
-            // 
-            // button8
-            // 
-            this.button8.Location = new System.Drawing.Point(965, 251);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(217, 37);
-            this.button8.TabIndex = 14;
-            this.button8.Text = "button8";
-            this.button8.UseVisualStyleBackColor = true;
-            this.button8.Click += new System.EventHandler(this.button8_Click);
-            // 
-            // button9
-            // 
-            this.button9.Location = new System.Drawing.Point(965, 310);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(217, 37);
-            this.button9.TabIndex = 15;
-            this.button9.Text = "button9";
-            this.button9.UseVisualStyleBackColor = true;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(911, 200);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(48, 16);
-            this.label8.TabIndex = 16;
-            this.label8.Text = "ADEDİ";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(844, 262);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(115, 16);
-            this.label9.TabIndex = 17;
-            this.label9.Text = "ÜRETİM FİRMASI";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(802, 321);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(157, 16);
-            this.label10.TabIndex = 18;
-            this.label10.Text = "SON KULLANMA TARİHİ";
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(595, 19);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(134, 16);
-            this.label11.TabIndex = 19;
-            this.label11.Text = "ECZANE İLAÇ KAYDI";
-            this.label11.Click += new System.EventHandler(this.label11_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(645, 61);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 16);
-            this.label2.TabIndex = 20;
-            this.label2.Text = "İLAC";
-            // 
-            // button10
-            // 
-            this.button10.Location = new System.Drawing.Point(521, 84);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(277, 219);
-            this.button10.TabIndex = 21;
-            this.button10.Text = "button10";
-            this.button10.UseVisualStyleBackColor = true;
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1274, 549);
-            this.Controls.Add(this.button10);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label11);
-            this.Controls.Add(this.label10);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.button9);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.sıra_sys);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(1292, 549);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -290,28 +170,20 @@
 
         #endregion
 
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Label sıra_sys;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- .github/aley/aley/Form1.cs
+++ .github/aley/aley/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace aley
 {
@@ -16,38 +17,75 @@ namespace aley
         {
             InitializeComponent();
         }
+        SqlConnection baglan = new SqlConnection("data source = ANYELA ; Initial Catalog = Eczane_DB;Integrated Security=true");
 
-        private void button1_Click(object sender, EventArgs e)
+        private void verilerigörüntüle()
         {
-            this.Close();
+            baglan.Open();
+            SqlCommand komut = new SqlCommand("select *from ilaclar", baglan);
+            SqlDataReader oku = komut.ExecuteReader();
+            while(oku.Read())
+            {
+                ListViewItem ekle =new ListViewItem();
+                //ekle.SubItems.Add(oku["sırano"].ToString());
+                ekle.SubItems.Add(oku["ilackodu"].ToString());
+                ekle.SubItems.Add(oku["ilacadı"].ToString());
+                ekle.SubItems.Add(oku["sonkullanmatarihi"].ToString());
+                //ekle.SubItems.Add(oku["barkodno"].ToString());
+                ekle.SubItems.Add(oku["fiyat"].ToString());
+                ekle.SubItems.Add(oku["adet"].ToString());
+                ekle.SubItems.Add(oku["üretimfirması"].ToString());
+                ekle.SubItems.Add(oku["kullanmatalimatı"].ToString());
+
+            }
+            baglan.Close();
+        }
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    this.Close();
+        //}
+
+        //private void label1_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
+        //private void button2_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
+        //private void button8_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
+        //private void label11_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
+        //private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        //{
+
+        //}
+
+        //private void button6_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
+        //private void sıra_sys_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            verilerigörüntüle();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button8_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label11_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-
-        }
-
-        private void button6_Click(object sender, EventArgs e)
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
